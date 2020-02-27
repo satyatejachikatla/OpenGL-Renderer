6df1bf6447e2c610a4e84b1313853567ba36628e
turrentrock <turrentrock@gmail.com>
2020-02-27 21:31:51 +0530

    Added Multiple Object Render

--- main.cpp
+++ main.cpp
@@ -62,10 +62,10 @@ int main(void) {
 
 	int positions_n = 4*4;
 	float positions[] = {
-		100.0f, 100.0f, 0.0f, 0.0f, //0
-		200.0f, 100.0f, 1.0f, 0.0f, //1
-		200.0f, 200.0f, 1.0f, 1.0f, //2
-		100.0f, 200.0f, 0.0f, 1.0f, //3
+		-50.0f, -50.0f, 0.0f, 0.0f, //0
+		 50.0f, -50.0f, 1.0f, 0.0f, //1
+		 50.0f,  50.0f, 1.0f, 1.0f, //2
+		-50.0f,  50.0f, 0.0f, 1.0f, //3
 	};
 
 	unsigned int indices_n = 6;
@@ -75,7 +75,7 @@ int main(void) {
 	};
 
 	glm::mat4 proj  = glm::ortho(0.0f,960.0f,0.0f,540.0f,-1.0f,1.0f);
-	glm::mat4 view  = glm::translate(glm::mat4(1.0f),glm::vec3(-100,0,0));
+	glm::mat4 view  = glm::translate(glm::mat4(1.0f),glm::vec3(0,0,0));
 //	glm::mat4 model = glm::translate(glm::mat4(1.0f),glm::vec3(200,200,0));
 //	glm::mat4 mvp = proj * view * model;
 
@@ -116,7 +116,8 @@ int main(void) {
 	ImGui_ImplOpenGL3_Init("#version 130");
 
 
-	glm::vec3 translation(200,200,0);
+	glm::vec3 translationA(200,200,0);
+	glm::vec3 translationB(400,200,0);
 
 	float r = 0.0f;
 	float increment = 0.05f;
@@ -129,16 +130,25 @@ int main(void) {
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		glm::mat4 model = glm::translate(glm::mat4(1.0f),translation);
-		glm::mat4 mvp = proj * view * model;
-
 		//Update Shader//
 		shader.Bind();
 		shader.SetUniform4f("u_Color",r ,0.3f,0.8f,1.0f);
-		shader.SetUniform4f("u_Color",0.8f,0.3f,0.8f,1.0f);
-		shader.SetUniformMat4f("u_MVP",mvp);
 
-		renderer.Draw(va,ib,shader);
+		{
+			glm::mat4 model = glm::translate(glm::mat4(1.0f),translationA);
+			glm::mat4 mvp = proj * view * model;
+			shader.Bind();
+			shader.SetUniformMat4f("u_MVP",mvp);
+			renderer.Draw(va,ib,shader);
+		}
+
+		{
+			glm::mat4 model = glm::translate(glm::mat4(1.0f),translationB);
+			glm::mat4 mvp = proj * view * model;
+			shader.Bind();
+			shader.SetUniformMat4f("u_MVP",mvp);
+			renderer.Draw(va,ib,shader);
+		}
 
 		if(r > 1.0f)
 			increment = -0.05f;
@@ -148,7 +158,8 @@ int main(void) {
 		r += increment;
 
 		{
-			ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+			ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);
+			ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 		}
 
